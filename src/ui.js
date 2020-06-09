bdd22a0302bf61a5bb295b7a891d7517e85fda54
bren2102 <brb.yg.21@gmail.com>
2020-06-09 16:27:40 -0500

    Add todo list per project functionality

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,6 @@
 import Projects from './initialize';
 import Project from './model/project';
+import Todo from './model/todo';
 
 const uiManager = (() => {
 
@@ -8,6 +9,7 @@ const uiManager = (() => {
   const todosList = document.getElementById('todos_list');
   const headerProjectName = document.querySelector('#header-project-name');
   const date = document.querySelector('#date');
+  const showAddInput = document.createElement('div');
 
   const initializeUi = () => {
     date.textContent = new Date().toDateString();
@@ -44,10 +46,59 @@ const uiManager = (() => {
       //title, description, dueDate, priority
       let todo = document.createElement('li');
       todo.classList = 'todo';
-      todo.innerHTML = `<div class="check-done"></div>${todoElement.title}`;
+      todo.innerHTML = `<div><div class="check-done"></div>
+                <span>${todoElement.title}</span></div>
+                <span>${todoElement.description}</span>
+                <span>${todoElement.dueDate}</span>
+                <span>${todoElement.priority}</span>`;
       todosList.appendChild(todo);
     });
+    addTodolist(project);
   };
+
+  const addTodolist = (currentProject) => {
+    const buttonAddtodo = document.createElement('button');
+    buttonAddtodo.innerHTML = `+ Add Todo`;
+    todosList.appendChild(buttonAddtodo);
+    const showAddInput = document.createElement('div');
+    buttonAddtodo.addEventListener('click', () => {
+      todosList.appendChild(showAddInput);
+      buttonAddtodo.remove();
+      const inputTodoTitle = document.createElement('input');
+      inputTodoTitle.type = 'text';
+      showAddInput.appendChild(inputTodoTitle);
+      const inputTodoDesc = document.createElement('input');
+      inputTodoDesc.type = 'text';
+      showAddInput.appendChild(inputTodoDesc);
+      const inputTodoDate = document.createElement('input');
+      inputTodoDate.type = 'date';
+      showAddInput.appendChild(inputTodoDate);
+      const inputTodoPriority = document.createElement('select');
+      inputTodoPriority.innerHTML = `<option value="high">High</option>
+                                <option value = "medium"> Medium </option>
+                                <option value = "low"> Low </option>`
+      showAddInput.appendChild(inputTodoPriority);
+      const buttonSubmitTodo = document.createElement('button');
+      buttonSubmitTodo.textContent = 'Add';
+      showAddInput.appendChild(buttonSubmitTodo);
+      const buttonCancelTodo = document.createElement('button');
+      buttonCancelTodo.textContent = 'Cancel';
+      showAddInput.appendChild(buttonCancelTodo);
+
+      buttonSubmitTodo.addEventListener('click', () => {
+        const newTodo = new Todo(inputTodoTitle.value, inputTodoDesc.value, inputTodoDate.value, inputTodoPriority.value, currentProject);
+        currentProject.addTodo(newTodo);
+        renderTodos(currentProject);
+        showAddInput.remove();
+        todosList.appendChild(buttonAddtodo);
+      });
+      buttonCancelTodo.addEventListener('click', () => {
+        showAddInput.remove();
+        todosList.appendChild(buttonAddtodo);
+      });
+    });
+  };
+
   const renderProjects = (projects) => {
     listProjects.innerHTML = '';
     projects.forEach(project => {
