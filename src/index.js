837288407973e5dcf15dc9f9cce737b61779510b
xtrmdarc <diegoreyesco@hotmail.com>
2020-06-09 11:49:53 -0500

    Add listeners to create projects and display their todos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Project from "./model/project";
 import Todo from "./model/todo";
 import Ui from "./ui.js"
+import Projects from './initialize';
 import './assets/styles/styles.scss';
+
+Ui.initializeUi();
 
 const project = new Project('test');
 const todo = new Todo('title', 'description', 'dueDate', 'priority', project);
 project.addTodo(todo);
+Projects.push(project);
 Ui.renderProjects([project]);
 Ui.renderTodos(project);
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,45 @@
+import Projects from './initialize';
+import Project from './model/project';
+
 const uiManager = (() => {
+
+  const buttonAddProject = document.querySelector('#addProjectButton');
+  const listProjects = document.getElementById('list-projects');
+  const todosList = document.getElementById('todos_list');
+  const headerProjectName = document.querySelector('#header-project-name');
+  const date = document.querySelector('#date');
+
+  const initializeUi = () => {
+    date.textContent = new Date().toDateString();
+  };
+
+  buttonAddProject.addEventListener('click',() => {
+
+    let li_project = document.createElement('li');
+    let newProjectInput = document.createElement('input');
+    newProjectInput.type = 'text';
+    newProjectInput.classList.add('new-project-name');
+    newProjectInput.placeholder = 'Add new project title';
+    newProjectInput.addEventListener('keypress',(e) => {
+      if(e.code === 'Enter')
+      {
+        let newProject = new Project(newProjectInput.value);
+        Projects.push(newProject);
+        newProjectInput.remove();
+        renderProjects(Projects);
+      }
+    });
+    
+    newProjectInput.focus();
+
+    //Add to DOM
+    li_project.appendChild(newProjectInput);
+    listProjects.appendChild(li_project);
+  });
+
   const renderTodos = (project) => {
-    let todosList = document.getElementById('todos_list');
+    todosList.innerHTML = '';
+    headerProjectName.textContent = project.name.charAt(0).toUpperCase() + project.name.slice(1);
     project.todoList.forEach(todoElement => {
       //title, description, dueDate, priority
       let todo = document.createElement('li');
@@ -10,9 +49,12 @@ const uiManager = (() => {
     });
   };
   const renderProjects = (projects) => {
-    let listProjects = document.getElementById('list-projects');
+    listProjects.innerHTML = '';
     projects.forEach(project => {
       let projList = document.createElement('li');
+      projList.addEventListener('click',() => {
+        renderTodos(project);
+      });
       let bullet = document.createElement('div');
       bullet.classList = 'li_bullet';
       projList.appendChild(bullet);    
@@ -20,6 +62,6 @@ const uiManager = (() => {
       listProjects.appendChild(projList);
     });
   };
-  return { renderTodos, renderProjects }
+  return { renderTodos, renderProjects, initializeUi }
 })();
 export default uiManager;
